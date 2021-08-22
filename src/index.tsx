071861f2673f4e08f9831632080a3c97354dbfe8
Jason Nickel <mail@devatrox.de>
2021-08-22 18:44:27 +0200

    add comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Svg from "@primer/octicons/build/svg/archive-24.svg";
-// import Svg from "./archive-24.svg";
+import Svg from "@primer/octicons/build/svg/archive-24.svg"; // does not work
+// import Svg from "./archive-24.svg"; // works
 
 ReactDOM.render(
     <Svg />,
